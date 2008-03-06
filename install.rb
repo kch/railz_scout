303185ebdcc4e878da461d84dcf921524a9e6a51
Caio Chassot <dev@caiochassot.com>
2008-03-06 23:32:03 +0000

    installer script

--- install.rb
+++ install.rb
@@ -1,3 +1,4 @@
 this_path = File.dirname(__FILE__)
 initializer_template_path = this_path + "/install/initializer.rb"
-initializer_destination = RAILS_ROOT + ""
+initializer_destination = RAILS_ROOT + "/config/initializers/railz_scout.rb"
+FileUtils.cp initializer_template_path, initializer_destination
