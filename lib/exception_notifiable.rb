50afd9e9958799dbba75046df998299611c761b3
Caio Chassot <dev@caiochassot.com>
2008-04-24 10:16:06 +0000

    get updates from exception_notifier plugin

--- lib/exception_notifiable.rb
+++ lib/exception_notifiable.rb
@@ -46,44 +46,49 @@ module ExceptionNotifiable
         write_inheritable_attribute(:exception_data, deliverer)
       end
     end
-  end
 
-  def local_request?
-    remote = IPAddr.new(request.remote_ip)
-    !self.class.local_addresses.detect { |addr| addr.include?(remote) }.nil?
-  end
-
-  def render_404
-    respond_to do |type|
-      type.html { render :file => "#{RAILS_ROOT}/public/404.html", :status => "404 Not Found" }
-      type.all  { render :nothing => true, :status => "404 Not Found" }
+    def exceptions_to_treat_as_404
+      RailzScout.config[:ignore_exceptions]
     end
   end
 
-  def render_500
-    respond_to do |type|
-      type.html { render :file => "#{RAILS_ROOT}/public/500.html", :status => "500 Error" }
-      type.all  { render :nothing => true, :status => "500 Error" }
+  private
+
+    def local_request?
+      remote = IPAddr.new(request.remote_ip)
+      !self.class.local_addresses.detect { |addr| addr.include?(remote) }.nil?
     end
-  end
 
-  def rescue_action_in_public(exception)
-    case exception
-      when *RailzScout.config[:ignore_exceptions]
-        render_404
-
-      else          
-        render_500
-
-        deliverer = self.class.exception_data
-        data = case deliverer
-          when nil then {}
-          when Symbol then send(deliverer)
-          when Proc then deliverer.call(self)
-        end
-
-        RailzScout.submit_bug(exception, self, request, data)
+    def render_404
+      respond_to do |type|
+        type.html { render :file => "#{RAILS_ROOT}/public/404.html", :status => "404 Not Found" }
+        type.all  { render :nothing => true, :status => "404 Not Found" }
+      end
     end
-  end
 
+    def render_500
+      respond_to do |type|
+        type.html { render :file => "#{RAILS_ROOT}/public/500.html", :status => "500 Error" }
+        type.all  { render :nothing => true, :status => "500 Error" }
+      end
+    end
+
+    def rescue_action_in_public(exception)
+      case exception
+        when *self.class.exceptions_to_treat_as_404
+          render_404
+
+        else          
+          render_500
+
+          deliverer = self.class.exception_data
+          data = case deliverer
+            when nil then {}
+            when Symbol then send(deliverer)
+            when Proc then deliverer.call(self)
+          end
+
+          RailzScout.submit_bug(exception, self, request, data)
+      end
+    end
 end
